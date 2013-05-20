49b1e9f3db5c70ef62ac53ad8c9a0fd876a9b2ae
Gueust <jeanbaptiste.lespiau@gmail.com>
2013-05-20 19:43:40 +0200

    Refactoring: The junction solving is now in the junction class

--- src/generalLWRNetwork/LWR_network.java
+++ src/generalLWRNetwork/LWR_network.java
@@ -66,13 +66,14 @@ public class LWR_network {
       junctions[g.junctions[i].getUniqueId()] = g.junctions[i];
     }
 
-    check();
-
     sources = g.sources.clone();
     sinks = g.destinations.clone();
 
     internal_split_ratios = g.split_ratios;
+
     nb_commodities = g.nb_paths;
+
+    check();
   }
 
   private void check() {
@@ -86,6 +87,9 @@ public class LWR_network {
       assert junctions[i].getNext() != null;
       assert junctions[i].getNext().length > 0 : "A junction has no outgoing cell !";
     }
+
+    /* We check that the internal_split_ratios is defined */
+    assert (internal_split_ratios != null);
   }
 
   public void print() {
@@ -238,135 +242,18 @@ public class LWR_network {
       p.get(cell_id).clearFlow();
     }
 
-    /* Computation of the flows */
-    // TODO: Put this in the junctions
-    double flow;
-    Junction j;
-    Cell[] previous_cells, next_cells;
+    /*
+     * Computation of the flows. The flows should have been cleared BEFORE doing
+     * this operation
+     */
+    // TODO: Check this is ok
     for (int j_id = 0; j_id < junctions.length; j_id++) {
+      // j = junctions[j_id];
+      /* We get the split-ratios for this junction at this time-step */
+      JunctionSplitRatios junction_sr =
+          internal_split_ratios.get(time_step, j_id);
+      junctions[j_id].solveJunction(p, time_step, junction_sr, cells);
 
-      j = getJunction(j_id);
-      previous_cells = j.getPrev();
-      next_cells = j.getNext();
-
-      // 1x1 Junctions
-      if (previous_cells.length == 1 && next_cells.length == 1) {
-        CellInfo previous = p.get(j.getPrev()[0]);
-        CellInfo next = p.get(j.getNext()[0]);
-        flow = Math.min(next.supply, previous.demand);
-        if (j_id == 0) {
-          System.out.println("Flow in Junction 0 at time step " + time_step
-              + ": " + flow);
-          previous.updateOutFlows(flow);
-          System.out.println("Previous cellInfo ");
-          previous.print();
-        }
-        next.updateInFlows(flow);
-      } else if (previous_cells.length == 1) {
-
-        /*
-         * i = previous_cells[0] is the incoming cell and j an outgoing cell at
-         * the studied junction
-         * We first compute for all flow_out_(i=0, k).
-         * Then we compute flow_out (i,c,k) and flow_out(j,c,k)
-         * If it is not zero we save it in the corresponding cells
-         */
-        /* We have: flow_out(i,j) = min (supply_j / beta(i,j), demand(i) */
-
-        /* This saves the beta(i, j, c) */
-        LinkedHashMap<Integer, Double> beta_i_j =
-            new LinkedHashMap<Integer, Double>(next_cells.length);
-
-        CellInfo cell_i = p.get(previous_cells[0]);
-
-        double flow_out = cell_i.demand;
-
-        /* If there is no no demand, there is no flow_in and out */
-        if (flow_out == 0)
-          continue;
-
-        JunctionSplitRatios junction_sr =
-            internal_split_ratios.get(time_step, j_id);
-        Iterator<Entry<Triplet, Double>> iterator =
-            junction_sr.compliant_split_ratios
-                .entrySet()
-                .iterator();
-        Entry<Triplet, Double> entry;
-        Triplet triplet;
-        Double beta_ijc, previous_beta;
-        Double density_ic;
-        Integer out_id;
-        /* Calculation of the beta(i, j) */
-        while (iterator.hasNext()) {
-          entry = iterator.next();
-          triplet = entry.getKey();
-          beta_ijc = entry.getValue();
-
-          density_ic = cell_i.partial_densities.get(triplet.commodity);
-          if (density_ic == null)
-            continue;
-
-          out_id = new Integer(triplet.outgoing);
-          previous_beta = beta_i_j.get(out_id);
-          if (previous_beta == null)
-            beta_i_j.put(out_id, density_ic * beta_ijc);
-          else
-            beta_i_j.put(out_id, previous_beta + density_ic * beta_ijc);
-        }
-
-        Iterator<Entry<Integer, Double>> iterator_beta = beta_i_j
-            .entrySet()
-            .iterator();
-        Entry<Integer, Double> beta_entry;
-        double density_i = cell_i.total_density;
-        assert density_i > 0;
-        double beta_ij_dividedby_density;
-
-        /* We compute flow_out(i,k) */
-        while (iterator_beta.hasNext()) {
-          beta_entry = iterator_beta.next();
-          beta_ij_dividedby_density = beta_entry.getValue() / density_i;
-          assert beta_entry.getValue() > 0;
-          beta_i_j.put(beta_entry.getKey(), beta_ij_dividedby_density);
-          assert beta_ij_dividedby_density > 0;
-
-          flow_out = Math.min(flow_out,
-              p.get(cells[beta_entry.getKey()]).supply
-                  / beta_ij_dividedby_density);
-        }
-
-        Iterator<Entry<Integer, Double>> iterator_partial_densities =
-            cell_i.partial_densities.entrySet().iterator();
-        Entry<Integer, Double> entry_density;
-        double flow_out_dividedby_density = flow_out / density_i;
-        double out_flow_for_commodity;
-        while (iterator_partial_densities.hasNext()) {
-          entry_density = iterator_partial_densities.next();
-
-          /* We compute flow_out(i,c,k) */
-          out_flow_for_commodity = flow_out_dividedby_density
-              * entry_density.getValue();
-          cell_i.out_flows.put(entry_density.getKey(), out_flow_for_commodity);
-
-          for (int out = 0; out < next_cells.length; out++) {
-            /* We compute flow_in(j,c,k) */
-            beta_ijc = junction_sr.get(previous_cells[0].getUniqueId(),
-                next_cells[out].getUniqueId(),
-                entry_density.getKey());
-            if (beta_ijc == null)
-              continue;
-            else {
-              p.get(next_cells[out]).in_flows.put(entry_density.getKey(),
-                  beta_ijc * out_flow_for_commodity);
-            }
-          }
-
-        }
-
-      } else {
-        System.out.println("Only 1x1 and 1xN junctions are working for now");
-        System.exit(1);
-      }
     }
 
     /* Creation of the new profile with the new densities */
