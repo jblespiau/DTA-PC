36bd4b8940d23a8be61ee6e42377c699d157e24e
Gueust <jeanbaptiste.lespiau@gmail.com>
2013-08-08 21:11:59 +0200

    Loading the network from the model object of PATH

--- src/DTASolver.java
+++ src/DTASolver.java
@@ -1,12 +1,18 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.Map.Entry;
 
+import org.apache.commons.io.FileUtils;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 
 import optimization.GradientDescent;
 import optimization.GradientDescentMethod;
-import optimization.LineSearch;
 import generalLWRNetwork.Cell;
 import generalLWRNetwork.Destination;
 import generalLWRNetwork.Junction;
@@ -18,27 +24,47 @@ import generalNetwork.data.Json_data;
 import generalNetwork.data.demand.Demands;
 import generalNetwork.data.demand.DemandsFactory;
 import generalNetwork.graph.DisplayGUI;
+import generalNetwork.graph.MutableGraph;
 import generalNetwork.graph.json.JsonFactory;
 import generalNetwork.state.State;
 import generalNetwork.state.internalSplitRatios.IntertemporalSplitRatios;
 import graphics.GUI;
 
+import dataStructures.HashMapPairCellsDouble;
+import dataStructures.PairCells;
 import dta_solver.Discretization;
 import dta_solver.SOPC_Optimizer;
 import dta_solver.SO_OptimizerByFiniteDifferences;
 import dta_solver.Simulator;
+import edu.berkeley.path.model_objects.MOException;
+
+import edu.berkeley.path.model_objects.jaxb.DemandProfile;
+import edu.berkeley.path.model_objects.jaxb.Link;
+import edu.berkeley.path.model_objects.jaxb.FundamentalDiagramProfile;
+import edu.berkeley.path.model_objects.jaxb.NetworkSet;
+import edu.berkeley.path.model_objects.jaxb.Node;
+
+import edu.berkeley.path.model_objects.scenario.Density;
+import edu.berkeley.path.model_objects.scenario.Scenario;
+import edu.berkeley.path.model_objects.scenario.ScenarioFactory;
+import edu.berkeley.path.model_objects.scenario.Splitratio;
+import edu.berkeley.path.model_objects.util.Serializer;
 
 public class DTASolver {
 
   /**
    * @param args
+   * @throws MOException
+   * @throws IOException
    */
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException, MOException {
     // EditorGUI e = new EditorGUI();
     // reportExample();
     // complexExample();
 
-    ZiliaskopoulosNetwork();
+    // ZiliaskopoulosNetwork();
+
+    highwayNetwork();
   }
 
   public static void printExample() {
@@ -329,4 +355,233 @@ public class DTASolver {
      */
   }
 
+  /**
+   * @brief Running the optimization from a network from PATH
+   * @details PATH code being closed, I have only added my interface to the git.
+   * The following jar files are needed to be able to run it: commons-io.1.4.jar
+   * model-objects-0.1-SNAPSHOT.jar, CORE-01-SNAPSHOT.jar, jettison-1.3.2.jar
+   */
+  public static void highwayNetwork() throws IOException, MOException {
+
+    /* Reset the unique id generators for cells and junctions */
+    NetworkUIDFactory.resetCell_id();
+    NetworkUIDFactory.resetJunction_id();
+
+    /*
+     * We first need to import the network and data into a usable format.
+     * To do so, we need to dig into the model object and grope for a working
+     * method
+     */
+    System.out.print("Loading PATH xml file...");
+    String xml = FileUtils.readFileToString(new File(
+        "graphs/Rerouting_plus_BF_SR_InD_v3.xml"));
+    Scenario scenario =
+        Serializer.xmlToObject(xml, Scenario.class, new ScenarioFactory());
+    System.out.println("Done.");
+
+    System.out.print("Converting it into an usable network...");
+    // We get the network
+    NetworkSet network_set = scenario.getNetworkSet();
+    List<edu.berkeley.path.model_objects.jaxb.Network> list = network_set
+        .getNetwork();
+    assert list.size() == 1;
+    edu.berkeley.path.model_objects.jaxb.Network network = list.get(0);
+
+    // We get the nodes
+    Iterator<edu.berkeley.path.model_objects.jaxb.Node> node_iterator =
+        network.getNodeList().getNode().iterator();
+    HashMap<Integer, edu.berkeley.path.model_objects.jaxb.Node> PATH_nodes =
+        new HashMap<Integer, edu.berkeley.path.model_objects.jaxb.Node>(
+            network.getNodeList().getNode().size());
+    edu.berkeley.path.model_objects.jaxb.Node tmp_node;
+    while (node_iterator.hasNext()) {
+      tmp_node = node_iterator.next();
+      PATH_nodes.put((Integer) (int) tmp_node.getId(), tmp_node);
+    }
+
+    // We get the links
+    Iterator<edu.berkeley.path.model_objects.jaxb.Link> link_iterator =
+        network.getLinkList().getLink().iterator();
+    HashMap<Integer, edu.berkeley.path.model_objects.jaxb.Link> PATH_links =
+        new HashMap<Integer, edu.berkeley.path.model_objects.jaxb.Link>(
+            network.getLinkList().getLink().size());
+    edu.berkeley.path.model_objects.jaxb.Link tmp_link;
+    while (link_iterator.hasNext()) {
+      tmp_link = link_iterator.next();
+      PATH_links.put((Integer) (int) tmp_link.getId(), tmp_link);
+    }
+
+    // We get the initial densities
+    Iterator<Density> densities =
+        scenario.getInitialDensitySet().getListOfDensities().iterator();
+
+    HashMap<Integer, Double> initial_densities =
+        new HashMap<Integer, Double>(10);
+    Density tmp_density;
+    while (densities.hasNext()) {
+      tmp_density = densities.next();
+      initial_densities.put((Integer) (int) tmp_density.getLinkId(),
+          Double.parseDouble(tmp_density.getContent()));
+    }
+    System.out.println("Initial densities:" + initial_densities.toString());
+
+    // We get the non-compliant split ratios
+    assert scenario.getSplitRatioSet().getListOfSplitRatioProfiles().size() == 1 : "Zero or multiple split ratios profile not implemented yet";
+    double split_ratios_dt = scenario
+        .getSplitRatioSet()
+        .getListOfSplitRatioProfiles()
+        .get(0).getDt();
+    System.out.println("Split ratios (dt =" + split_ratios_dt + "):");
+
+    LinkedList<HashMapPairCellsDouble> SR_list =
+        new LinkedList<HashMapPairCellsDouble>();
+
+    Iterator<Splitratio> non_compliant_SR =
+        scenario
+            .getSplitRatioSet()
+            .getListOfSplitRatioProfiles()
+            .get(0)
+            .getListOfSplitratios()
+            .iterator();
+
+    Splitratio tmp_SR;
+    while (non_compliant_SR.hasNext()) {
+      tmp_SR = non_compliant_SR.next();
+      HashMapPairCellsDouble non_compliant_split_ratios;
+      if (Integer.parseInt(tmp_SR.getIds()) == 1) {
+        non_compliant_split_ratios = new HashMapPairCellsDouble();
+        SR_list.add(non_compliant_split_ratios);
+      } else {
+        non_compliant_split_ratios = SR_list.getLast();
+      }
+      non_compliant_split_ratios.put(
+          new PairCells((int) tmp_SR.getLinkIn(), (int) tmp_SR.getLinkOut()),
+          Double.parseDouble(tmp_SR.getContent()));
+    }
+
+    HashMapPairCellsDouble[] SR_array =
+        new HashMapPairCellsDouble[SR_list.size()];
+    SR_list.toArray(SR_array);
+
+    System.out
+        .println("Non-compliant split ratios:" + Arrays.toString(SR_array));
+
+    // Demand set
+    Iterator<DemandProfile> it_demand =
+        scenario.getDemandSet().getDemandProfile().iterator();
+    HashMap<Integer, Double> demands = new HashMap<Integer, Double>();
+
+    DemandProfile tmp_demand;
+    while (it_demand.hasNext()) {
+      tmp_demand = it_demand.next();
+      assert tmp_demand.getDemand().size() == 1 : "Dealing with constant demand";
+      demands.put((Integer) (int) tmp_demand.getLinkIdOrg(),
+          Double.parseDouble(tmp_demand.getDemand().get(0).getContent()));
+    }
+
+    System.out.println("Demand profiles:" + demands.toString());
+
+    // Fundamental Triangular Diagram
+    Iterator<edu.berkeley.path.model_objects.scenario.FundamentalDiagramProfile> it_fdp =
+        scenario
+            .getFundamentalDiagramSet()
+            .getListOfFundamentalDiagramProfiles()
+            .iterator();
+
+    HashMap<Integer, FundamentalDiagramProfile> fundamentalDiagrams =
+        new HashMap<Integer, FundamentalDiagramProfile>(
+            scenario
+                .getFundamentalDiagramSet()
+                .getListOfFundamentalDiagramProfiles().size());
+
+    FundamentalDiagramProfile tmp_FDP;
+    while (it_fdp.hasNext()) {
+      tmp_FDP = it_fdp.next();
+      fundamentalDiagrams.put((int) tmp_FDP.getId(), tmp_FDP);
+      /*
+       * double dt = tmp_FDP.getDt();
+       * FundamentalDiagram fundamental_diagram = tmp_FDP
+       * .getFundamentalDiagram()
+       * .get(0);
+       * double capacity = fundamental_diagram.getCapacity();
+       * double congestion_speed = fundamental_diagram.getCongestionSpeed();
+       * double free_flow_speed = fundamental_diagram.getFreeFlowSpeed();
+       */
+    }
+
+    /*
+     * We create the mutable graph:
+     * - We add the good number of nodes and keep the relation between PATH
+     * nodes and nodes
+     * - We add the good number of links and keep the relation between PATH
+     * links
+     * - We ensure the update of the pointers to nodes and links
+     */
+    MutableGraph mutable_graph = new MutableGraph();
+
+    /* We first add the nodes */
+    Iterator<Node> all_nodes = PATH_nodes.values().iterator();
+    HashMap<Integer, generalNetwork.graph.Node> PATHNode_to_nodes =
+        new HashMap<Integer, generalNetwork.graph.Node>(PATH_nodes.size());
+
+    while (all_nodes.hasNext()) {
+      tmp_node = all_nodes.next();
+      mutable_graph.addNode(0, 0);
+      PATHNode_to_nodes.put((int) tmp_node.getId(),
+          mutable_graph.getLastAddedNode());
+    }
+
+    /* We then add the links */
+    Iterator<Link> all_links = PATH_links.values().iterator();
+    HashMap<Integer, generalNetwork.graph.Link> PATHLink_to_links =
+        new HashMap<Integer, generalNetwork.graph.Link>(PATH_links.size());
+
+    while (all_links.hasNext()) {
+      tmp_link = all_links.next();
+      generalNetwork.graph.Node from, to;
+      from = PATHNode_to_nodes.get((int) tmp_link.getBegin().getNodeId());
+      assert from != null;
+      to = PATHNode_to_nodes.get((int) tmp_link.getEnd().getNodeId());
+      assert to != null;
+      mutable_graph.addLink(from, to);
+
+      PATHLink_to_links.put((int) tmp_link.getId(),
+          mutable_graph.getLastAddedLink());
+
+      generalNetwork.graph.Link tmp = mutable_graph.getLastAddedLink();
+      from.addOutgoingLink(tmp);
+      to.addIncomingLink(tmp);
+    }
+
+    assert mutable_graph.check() : "We should have nodes[i].id = i and links[i].id = i";
+    // Then we create all the nodes
+    // we create the hash map PATH_node -> JB_node
+    // We create all the links with the right values of the incoming links,
+    // We create the Hash map PATH_link -> JB_Link
+    // We update the correct in and out link in the jb-nodes.
+    // We create the graph.
+    // We add the non-compliant split ratios
+    // We also need to create the path !!!
+
+    // We also need to discretize the demand and the non-compliant split ratios
+
+    // Then the rest should be ok:
+    // - we decide of a value for delta_t
+    // - We discretize the network
+    // - build the simulator from it, ...
+
+    // the right fundamental diagram
+    // We get the fundamental triangular diagram
+    /*
+     * Iterator<edu.berkeley.path.model_objects.jaxb.Link> iterator =
+     * network.getLinkList().getLink().iterator();
+     * 
+     * edu.berkeley.path.model_objects.jaxb.Link tmp_link;
+     * while (iterator.hasNext()) {
+     * tmp_link = iterator.next();
+     * 
+     * }
+     */
+    System.out.println("Done.");
+  }
 }
