0fb0232c3606babe1decb249eac279ad0b5133bd
Gueust <jeanbaptiste.lespiau@gmail.com>
2013-08-13 03:08:33 +0200

    Added a missing file

---
+++ src/dataStructures/HashMapPairCellsDouble.java
@@ -0,0 +1,9 @@
+package dataStructures;
+
+import java.util.HashMap;
+
+public class HashMapPairCellsDouble extends HashMap<PairCells, Double> {
+
+  private static final long serialVersionUID = 1L;
+
+}
